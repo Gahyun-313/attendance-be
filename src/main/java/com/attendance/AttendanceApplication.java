0cbd133a62f86210bff3d2504f02ac1390b899ea
Gahyun-313 <gus20313@gmail.com>
2026-05-13 23:29:15 +0900

    chore: 프로젝트 초기 세팅

--- src/main/java/com/attendance/AttendanceApplication.java
+++ src/main/java/com/attendance/AttendanceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AttendanceBeApplication {
+public class AttendanceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AttendanceBeApplication.class, args);
+		SpringApplication.run(AttendanceApplication.class, args);
 	}
 
 }
